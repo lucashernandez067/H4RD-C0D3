9d0f85ef0aa54682eb20f4cad45fa80ba365ede5
Alexjh16 <Alexx.16@protonmail.com>
2019-09-28 17:08:25 -0500

    banner.php

--- views/layouts/banner.php
+++ views/layouts/banner.php
@@ -13,7 +13,6 @@
 				<li><a>Profesores</a></li>
 				<li><a>Estudiantes</a></li>
 				<li><a>Grados</a></li>
-				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 				<li><a class="login-button">Login</a></li>
 				<li><b>|</b></li>
 				<li><a class="sign-up-button">Sign up</a></li>
