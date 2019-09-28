ec1122f1a8619b3e616e7bb5943639a2a65e2d6b
Felipe Chacon <felipipe88806@gmail.com>
2019-09-28 13:43:02 -0500

    dev

--- views/Index/index.php
+++ views/Index/index.php
@@ -3,7 +3,8 @@
 <body class="index-body">
     <?php include('views/layouts/banner.php') ?>
 
-      
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam nisi id animi atque nostrum aperiam est omnis itaque asperiores, quos quisquam veritatis ducimus. Vero perferendis, pariatur ea nulla mollitia aspernatur.
+    <div style="margin:1000px;"></div>  
 
 
     <?php include('views/layouts/footer.php') ?>
