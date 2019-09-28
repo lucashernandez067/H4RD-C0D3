8e0cd0590e641b7693f791c88d3d81f57bc9fa56
Alexjh16 <Alexx.16@protonmail.com>
2019-09-28 16:53:28 -0500

    fixed header.php

--- views/layouts/banner.php
+++ views/layouts/banner.php
@@ -1,9 +1,12 @@
 <header>
 <nav>
-	<ul>		
+	<ul>
 	<div class="container">
 		<div class="row">
-			<div class="col-md-3"></div>
+			<div class="col-md-5">
+				<li><span>STUDENT_STUDY</span></li>
+			</div>
+			<div class="col-md-1"></div>
 			<div class="col-md-6">
 				<li><a>Profesores</a></li>
 				<li><a>Estudiantes</a></li>
