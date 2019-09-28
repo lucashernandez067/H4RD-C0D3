1e3b9b75be2a7ac15d6af755f72057bccb8f6540
Alexjh16 <Alexx.16@protonmail.com>
2019-09-28 17:35:53 -0500

    no problem

--- views/Index/index.php
+++ views/Index/index.php
@@ -2,12 +2,47 @@
 <title>Inicio</title>
 <body class="">
     <?php include('views/layouts/banner.php') ?>
-<section>
-    <div class="banner"></div>
-    <div>
-        <h2>lorem</h2>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <section>
+                <div>
+                    <h2 class="text-center">Grados Generales</h2>
+                </div>
+            </section>
+        </div>
     </div>
-</section>
+    <div class="row">
+        <div class="col-md-6">
+            <div>
+                <img class="img-responsive" src="assets/img/kinderB.jpg" alt="">
+            </div>
+        </div>
+        <div class="col-md-6">
+            <h4>Cursos En primarias</h4>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus consectetur obcaecati voluptatum mollitia laborum, similique cumque impedit illum eum aliquid, cupiditate pariatur, explicabo dolor. Obcaecati consequuntur nulla officiis soluta nihil.
+
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus consectetur obcaecati voluptatum mollitia laborum, similique cumque impedit illum eum aliquid, cupiditate pariatur, explicabo dolor. Obcaecati consequuntur nulla officiis soluta nihil.
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus consectetur obcaecati voluptatum mollitia laborum, similique cumque impedit illum eum aliquid, cupiditate pariatur, explicabo dolor. Obcaecati consequuntur nulla officiis soluta nihil.
+        </div>
+    </div>
+    <hr>
+    <div class="row">
+        <div class="col-md-6">
+            <h4>Cursos En primarias</h4>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus consectetur obcaecati voluptatum mollitia laborum, similique cumque impedit illum eum aliquid, cupiditate pariatur, explicabo dolor. Obcaecati consequuntur nulla officiis soluta nihil.
+
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus consectetur obcaecati voluptatum mollitia laborum, similique cumque impedit illum eum aliquid, cupiditate pariatur, explicabo dolor. Obcaecati consequuntur nulla officiis soluta nihil.
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus consectetur obcaecati voluptatum mollitia laborum, similique cumque impedit illum eum aliquid, cupiditate pariatur, explicabo dolor. Obcaecati consequuntur nulla officiis soluta nihil.
+            <br>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus consectetur obcaecati voluptatum mollitia laborum, similique cumque impedit illum eum aliquid, cupiditate pariatur, explicabo dolor. Obcaecati consequuntur nulla officiis soluta nihil.        </div>
+        <div class="col-md-6">
+            <div>
+                <img class="img-responsive" src="assets/img/child.jpg" alt="">
+            </div>
+        </div>
+    </div>
+</div>
     <?php include('views/layouts/footer.php') ?>
 </body>
 </html>
