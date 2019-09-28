f1617e185d40e12782b3d01a317b1236c4f31461
Luis Carlos Hernández Peña <lucashernandez067@gmail.com>
2019-09-28 11:56:10 -0500

    Administradores y Estudiantes

---
+++ views/administradores/index.php
@@ -0,0 +1,6 @@
+<?php include('views/layouts/head.php'); ?>
+<title>Administrador</title>
+<body>
+    panel de Administrador :v
+</body>
+</html>
---
+++ views/estudiantes/index.php
@@ -0,0 +1,6 @@
+<?php include('views/layouts/head.php'); ?>
+<title>Estudiantes</title>
+<body>
+    Panel de Estudiantes ;v
+</body>
+</html>
