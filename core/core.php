a6d7339aa676e771ef11abfda51efa8bef04ef31
Luis Carlos Hernández Peña <lucashernandez067@gmail.com>
2019-09-28 11:00:24 -0500

    Security.php

--- core/core.php
+++ core/core.php
@@ -2,6 +2,6 @@
     require_once("models/DB.php");
     require_once('models/File.php');
     require_once('models/Index.php');
-    require_once('models/Securiy');
+    require_once('models/Securiy.php');
 ?>
 
