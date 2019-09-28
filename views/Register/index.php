187224f85b0433b7b96505ecf5d8cfca606ce7cc
DasheÃll2514 <Dashell2514@gmail.com>
2019-09-28 13:11:00 -0500

    register actualice

--- views/Register/index.php
+++ views/Register/index.php
@@ -11,61 +11,21 @@
     <title>Registro</title>
 </head>
 <body>
-<<<<<<< HEAD
 <div class="container">
 		<div class="form__top">
 			<h2>Formulario <span>Registro</span></h2>
 		</div>		
 		<form class="form__reg" action="">
-			<input class="input" type="text" placeholder="&#128100;  Nombre" required autofocus>
+            <input class="input" type="text" placeholder="&#128100;  Name" required autofocus>
+            <input class="input" type="text" placeholder="&#128090;   Last_name" required>
+            <input class="input" type="email" placeholder="&#128084;  Document" required>
+            <input class="input" type="email" placeholder="&#127760;  Password" required>
             <input class="input" type="email" placeholder="&#9993;  Email" required>
-            <input class="input" type="text" placeholder="&#128222;  Telefono" required>
-            <input class="input" type="text" placeholder="&#8962;  Dirección" required>
             <div class="btn__form">
-            	<input class="btn__submit" type="submit" value="REGISTRAR">
-            	<input class="btn__reset" type="reset" value="LIMPIAR">	
+            	<input class="btn__submit" type="submit" value="SignUp">
+            	<input class="btn__reset" type="reset" value="Clear">	
             </div>
 		</form>
 	</div>
-=======
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Register User</div>
-                        <div class="panel-body">
-                            <form action="?c=Register&m=registrar" method="post">
-                                <div>
-                                 <label for="">Name</label>
-                                 <input type="text" name="name" placeholder="Name" required>
-                                 </div>
-                                 <div>
-                                 <label for="">Last Name</label>
-                                 <input type="text" name="last_name" placeholder="Last_name">
-                                 </div>
-                                 <div>
-                                 <label for="">Document</label>
-                                 <input type="num" name="document" placeholder="Document" required>
-                                 </div>
-                                 <div>
-                                 <label for="">Password</label>
-                                 <input type="password" name="pass" placeholder="Password" required>
-                                 </div>
-                                 <div>
-                                 <label for="">Email</label>
-                                 <input type="text" name="email" placeholder="Email">
-                                 </div>
-                                 <div>
-                                   <input type="submit">  
-                                 </div>
-                             </form>
-                       
-                    </div>
-                </div>
-            </div>  
-        </div>
-    </div>
-    
->>>>>>> a048449ab1dfb3234b8252a205636e0f93cc5e8b
 </body>
 </html>
