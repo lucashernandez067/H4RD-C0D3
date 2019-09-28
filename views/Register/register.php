a8c4545f31020b9d09679ec01afdffb85e97ec11
Darkaro01 <48836530+Darkaro01@users.noreply.github.com>
2019-09-28 11:02:21 -0500

    register

---
+++ views/Register/register.php
@@ -0,0 +1,51 @@
+<!-- include('views/layouts/)-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+    <title>Registro</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Register User</div>
+                        <div class="panel-body">
+                            <form action="">
+                                <div>
+                                 <label for="">Name</label>
+                                 <input type="text" name="name" placeholder="Name" required>
+                                 </div>
+                                 <div>
+                                 <label for="">Last Name</label>
+                                 <input type="text" name="apellido" placeholder="Last_name">
+                                 </div>
+                                 <div>
+                                 <label for="">Document</label>
+                                 <input type="num" name="document" placeholder="Document">
+                                 </div>
+                                 <div>
+                                 <label for="">Password</label>
+                                 <input type="password" name="password" placeholder="Password">
+                                 </div>
+                                 <div>
+                                 <label for="">Email</label>
+                                 <input type="text" name="email" placeholder="Email">
+                                 </div>
+
+                             </form>
+                       
+                    </div>
+                </div>
+            </div>  
+        </div>
+    </div>
+    
+</body>
+</html>
