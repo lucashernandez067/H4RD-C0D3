66b5d5c9214f7bf490ecd6a862add0d1efda407a
Felipe Chacon <felipipe88806@gmail.com>
2019-09-28 10:57:13 -0500

    Add style.css

--- views/layouts/head.php
+++ views/layouts/head.php
@@ -5,4 +5,5 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="assets/css/bootstrap-3.3.7/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
