ec289a7fbf449e317bbf7dc7b4f2ad79febf0b72
Felipe Chacon <felipipe88806@gmail.com>
2019-09-28 16:10:33 -0500

    Prueba 1.0

--- views/estudiantes/index.php
+++ views/estudiantes/index.php
@@ -3,23 +3,52 @@
 <body>
 <?php include('views/layouts/banner.php'); ?>
 
-    <div class="container-fluid">
+    <div class="container">
         <div class="row">
-            <div class="col-md-4">
-                <div class="panel panel-default">
+           
+            <div class="col-md-9">
+            <iframe width="100%" height="500px" src="https://www.youtube.com/embed/I2F9Bbu89_E" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            
+            <div class="row">
+                <div class="col-md-10">
+            <h3>Fracciones</h3>
+            <hr>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem perferendis quos cupiditate officiis, temporibus saepe eveniet velit veniam modi quidem! Quaerat labore corporis eos! Optio sapiente iste soluta eligendi maxime. lorem lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse fugit magni doloremque libero voluptatum ea sed eligendi itaque distinctio. Nostrum eos consequatur consequuntur natus dolorum perspiciatis, molestiae soluta alias molestias! </p>
+            <a href=""> Ver Más</a>
+            <hr>
+                       
+            <h3>Comentarios</h3>
+            <hr>
+            <input type="text" name="" id="" class="form-control" style="height:100px;">
+            <button class="btn btn-success" style="margin-top:10px;">Enviar Comentario</button>
+            <hr>
+            <div>
+            
+            <h3><img src="assets/img/img01.png" alt="" width="10%"> Felipe</h3>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus veniam dolore autem illo aliquid, corporis labore consectetur expedita </p>
+            </div>
+            </div>
+                </div>
+            </div>
+
+            <div class="col-md-3">
+                <div class="panel panel-info">
                     <div class="panel-heading">
                         <h3>Matemáticas</h3>
                     </div>
                     <div class="panel-body">
-                        <ul>
-                            <li><a href="?c=Estudiantes&m=">Fracciones</a></li>
+                        <ol>
+                            <li><a href=""><h4>Fracciones</h4></a></li>
+                            <li><a href=""><h4>Numero Racionales</h4></a></li>
+                            <li><a href=""><h4>Expresiones Decimales</h4></a></li>
+                            <li><a href=""><h4>Ecuaciones con Enteros</h4></a></li>
+                            <li><a href=""><h4>Sistema de Numeración</h4></a></li>
 
-                        </ul>
+                        </ol>
                     </div>
                 </div>
-            </div>
-            <div class="col-md-8">
 
+                <a href="" class="btn btn-info btn-block btn-lg">PRESENTAR PRUEBA</a>
             </div>
         </div>
     </div>
