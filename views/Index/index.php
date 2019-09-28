ea8848e40ab3d91c10da0f02891d6c0003de6cc6
Felipe Chacon <felipipe88806@gmail.com>
2019-09-28 14:07:22 -0500

    registerM

--- views/Index/index.php
+++ views/Index/index.php
@@ -1,16 +1,15 @@
 <?php include('views/layouts/head.php'); ?>
 <title>Inicio</title>
-<body class="index-body">
+<body class="">
     <?php include('views/layouts/banner.php') ?>
 
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                
+                asdasdasd
             </div>
         </div>
     </div>  
-
 
     <?php include('views/layouts/footer.php') ?>
 </body>
