9a4026927a28094a0b8af39798c18bf20d4fae53
DasheÃll2514 <Dashell2514@gmail.com>
2019-09-28 12:05:58 -0500

    register view

--- controllers/RegisterController.php
+++ controllers/RegisterController.php
@@ -1,15 +1,7 @@
 <?php 
     class RegisterController extends Register{
-        
-        public function register(){
-        require_once('views/Register/index.php');
-    }
-        public function index(){
-            
-        }
         public function index(){
         require_once('views/Register/index.php');
         }
     }
- }
 ?>
