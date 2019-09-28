fb700b690b41f446658a8ac1920267d40bfe4c0d
Alexjh16 <Alexx.16@protonmail.com>
2019-09-28 10:50:08 -0500

    Create database.php file

---
+++ models/database.php
@@ -0,0 +1,15 @@
+<?php 
+class database{
+    public function connect(){
+        try{
+            $PDO = new PDO("mysql:host=localhost;dbname=database_hackatrix","root","");
+            $PDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $PDO;
+        }
+
+        catch(Exception $error){
+            die("Error foun in file models/database.php".$error->getMessage());
+        }
+    }
+}
+?>
