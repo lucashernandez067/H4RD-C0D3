1de56b2a17149e543e255a043ca939c52cfa7ebf
Luis Carlos Hernández Peña <lucashernandez067@gmail.com>
2019-09-28 17:15:55 -0500

    Materias +

--- controllers/MateriasController.php
+++ controllers/MateriasController.php
@@ -3,8 +3,26 @@
         public function matematicas(){
             require_once('views/materias/matematicas.php');
         }
-        public function espain(){
-            require_once('views/materias/matematicas.php');
+        public function spanish(){
+            require_once('views/materias/spanish.php');
+        }
+        public function sociales(){
+            require_once('views/materias/sociales.php');
+        }
+        public function ingles(){
+            require_once('views/materias/ingles.php');
+        }
+        public function quimica(){
+            require_once('views/materias/quimica.php');
+        }
+        public function fisica(){
+            require_once('views/materias/fisica.php');
+        }
+        public function etica(){
+            require_once('views/materias/etica.php');
+        }
+        public function religion(){
+            require_once('views/materias/religion.php');
         }
     }
 ?>
