1f4b372ee6310ab587381a44a9be5a9b9fe85d29
Felipe Chacon <felipipe88806@gmail.com>
2019-09-28 11:24:37 -0500

    Add Login

--- views/Index/index.php
+++ views/Index/index.php
@@ -1,10 +1,12 @@
 <?php include('views/layouts/head.php'); ?>
 <title>Inicio</title>
 <body>
+
+
     Hola En el index
-
-
     
+
+
     <?php include('views/layouts/footer.php') ?>
 </body>
 </html>
