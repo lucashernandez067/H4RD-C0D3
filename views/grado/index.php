11c42d9cbb3c3d6477ab6be084655820da546876
DasheÃll2514 <Dashell2514@gmail.com>
2019-09-28 14:26:43 -0500

    rgister and grados

---
+++ views/grado/index.php
@@ -0,0 +1,13 @@
+<?php include('views/layouts/banner.php'); ?>
+<?php include('views/layouts/head.php'); ?>
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                
+            </div>
+        </div>
+    </div>
+    <?php include('views/layouts/footer.php'); ?>
+</body>
+</html>
