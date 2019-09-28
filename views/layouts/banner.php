94bd595473493ce59999f793f0f66cc2838ead02
Alexjh16 <Alexx.16@protonmail.com>
2019-09-28 15:00:37 -0500

    index.php

--- views/layouts/banner.php
+++ views/layouts/banner.php
@@ -1,8 +1,23 @@
 <header>
 <nav>
 	<ul>
-		<li><a href="index.html">H4RD-C0D3</a></li>
-    </ul>
-		
+	<div class="conainer">
+		<div class="row">
+			<div class="col-md-4">
+				<li><span>STUDENT_STUDY</span></li>
+			</div>
+			<div class="col-md-2"></div>
+			<div class="col-md-5">
+				<li><a>Profesores</a></li>
+				<li><a>Estudiantes</a></li>
+				<li><a>Grados</a></li>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+				<li><a class="">Login</a></li>
+				<li><b>|</b></li>
+				<li><a class="">Sign up</a></li>
+			</div>
+		</div>
+	</div>		
+    </ul>		
 </nav>
 </header>
