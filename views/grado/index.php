04691f49c24728f35039c9f16926a9c773072ac9
DasheÃll2514 <Dashell2514@gmail.com>
2019-09-28 17:12:21 -0500

    macaco

--- views/grado/index.php
+++ views/grado/index.php
@@ -4,19 +4,144 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-12">
-                <h1 class="center">asdasds</h1>
+                <h1 class="text-center">STUDENT_STUDY</h1>
             </div>
         </div>
     </div>
+    <div class="back"><h2><marquee direction="left">PRIMARIA</marquee></h2></div>
     <div class="container">
         <div class="row">
-            <div class="col-md-3">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat doloremque modi magni alias neque nobis animi, pariatur commodi, odio blanditiis ullam! Tempora minima ea, numquam laudantium modi rem ex vitae!</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum sequi error tenetur nisi nostrum obcaecati sint dolor incidunt qui eos quos, sunt corporis a doloremque modi facere labore vitae voluptatem?</p>
+            <div class="col-md-4">
+                        <p class="numone">3</p>               
+                   <div>
+                       <table class="table table-bordered">
+                           <tr class="table-primary"><th class="center"><a href="?c=Materias&m=matematicas">Matematicas</a></th></tr>
+                           <tr><th class="center">Sociales</th><tr>
+                           <tr><th class="center">Español</th><tr>
+                           <tr><th class="center">Ingles</th><tr>
+                           <tr><th class="center">Religion</th><tr>
+                           <tr><th class="center">Etica</th><tr>
+                       </table>
+                   </div>
             </div>
-            <div class="col-md-3">
-               
+            <div class="col-md-4">
+                    <p class="numdos">4</p>
+                    <div>
+                       <table class="table table-bordered">
+                           <tr class="table-primary"><th class="text-center centerdos"> Matematicas</th></tr>
+                           <tr><th class="text-center centerdos">Sociales</th><tr>
+                           <tr><th class="text-center centerdos">Español</th><tr>
+                           <tr><th class="text-center centerdos">Ingles</th><tr>
+                           <tr><th class="text-center centerdos">Religion</th><tr>
+                           <tr><th class="text-center centerdos">Etica</th><tr>
+                       </table>
+                   </div>
             </div>
+            <div class="col-md-4">
+                    <p class="numtres">5</p>
+                    <div>
+                       <table class="table table-bordered">
+                           <tr class="table-primary"><th class="text-center centerone"> Matematicas</th></tr>
+                           <tr><th class="text-center centerone">Sociales</th><tr>
+                           <tr><th class="text-center centerone">Español</th><tr>
+                           <tr><th class="text-center centerone">Ingles</th><tr>
+                           <tr><th class="text-center centerone">Religion</th><tr>
+                           <tr><th class="text-center centerone">Etica</th><tr>
+                       </table>
+                   </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+            <div class="back"><h2><marquee direction="left"> MEDIA BACHILLERATO</marquee></h2></div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <p class="numone">6</p>
+                <div>
+                       <table class="table table-bordered">
+                           <tr class="table-primary"><th class="center"> Matematicas</th></tr>
+                           <tr><th class="center">Sociales</th><tr>
+                           <tr><th class="center">Español</th><tr>
+                           <tr><th class="center">Ingles</th><tr>
+                           <tr><th class="center">Religion</th><tr>
+                           <tr><th class="center">Etica</th><tr>
+                       </table>
+                   </div>
+            </div>
+            <div class="col-md-4">
+                <p class="numdos">7</p>
+                <div>
+                       <table class="table table-bordered">
+                           <tr class="table-primary"><th class="text-center centerdos"> Matematicas</th></tr>
+                           <tr><th class="text-center centerdos">Sociales</th><tr>
+                           <tr><th class="text-center centerdos">Español</th><tr>
+                           <tr><th class="text-center centerdos">Ingles</th><tr>
+                           <tr><th class="text-center centerdos">Religion</th><tr>
+                           <tr><th class="text-center centerdos">Etica</th><tr>
+                       </table>
+                   </div>
+            </div>
+            <div class="col-md-4">
+                <p class="numtres">8</p>
+                <div>
+                       <table class="table table-bordered">
+                           <tr class="table-primary"><th class="text-center centerone"> Matematicas</th></tr>
+                           <tr><th class="text-center centerone">Sociales</th><tr>
+                           <tr><th class="text-center centerone">Español</th><tr>
+                           <tr><th class="text-center centerone">Ingles</th><tr>
+                           <tr><th class="text-center centerone">Religion</th><tr>
+                           <tr><th class="text-center centerone">Etica</th><tr>
+                       </table>
+                   </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+            <div class="back"><h2><marquee direction="right">BACHILLERATO</marquee></h2></div>
+            </div>
+            <div class="row">
+            <div class="col-md-4">
+                <p class="numone">9</p>
+                <div>
+                       <table class="table table-bordered">
+                           <tr class="table-primary"><th class="center"> Matematicas</th></tr>
+                           <tr><th class="center">Sociales</th><tr>
+                           <tr><th class="center">Español</th><tr>
+                           <tr><th class="center">Ingles</th><tr>
+                           <tr><th class="center">Religion</th><tr>
+                           <tr><th class="center">Etica</th><tr>
+                       </table>
+                   </div>
+            </div>
+            <div class="col-md-4">
+                <p class="numdos">10</p>
+                <div>
+                       <table class="table table-bordered">
+                           <tr class="table-primary"><th class="text-center centerdos"> Matematicas</th></tr>
+                           <tr><th class="text-center centerdos">Sociales</th><tr>
+                           <tr><th class="text-center centerdos">Español</th><tr>
+                           <tr><th class="text-center centerdos">Ingles</th><tr>
+                           <tr><th class="text-center centerdos">Religion</th><tr>
+                           <tr><th class="text-center centerdos">Etica</th><tr>
+                       </table>
+                   </div>
+            </div>
+            <div class="col-md-4">
+                <p class="numtres">11</p>
+                <div>
+                       <table class="table table-bordered">
+                           <tr class="table-primary"><th class="text-center centerone"> Matematicas</th></tr>
+                           <tr><th class="text-center centerone">Sociales</th><tr>
+                           <tr><th class="text-center centerone">Español</th><tr>
+                           <tr><th class="text-center centerone">Ingles</th><tr>
+                           <tr><th class="text-center centerone">Religion</th><tr>
+                           <tr><th class="text-center centerone">Etica</th><tr>
+                       </table>
+                   </div>
+            </div>
+        </div>
         </div>
     </div>
     <?php include('views/layouts/footer.php'); ?>
