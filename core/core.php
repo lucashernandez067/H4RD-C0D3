1a04c3ca1290274d2e6354aed6da70740bcc5221
Alexjh16 <Alexx.16@protonmail.com>
2019-09-28 10:56:02 -0500

    fixed core/core.php

--- core/core.php
+++ core/core.php
@@ -1,9 +1,6 @@
 <?php
     require_once('models/File.php');
     require_once('models/Index.php');
-<<<<<<< HEAD
-?> 
-=======
     require_once('models/Securiry');
 ?>
->>>>>>> 215eaa132b9cbcaacb6555aa7a11bb5b53cd290a
+
