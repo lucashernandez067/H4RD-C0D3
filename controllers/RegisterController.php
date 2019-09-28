5f634b769ddc8bc699e9ef5a42b81090d041d1e2
Darkaro01 <48836530+Darkaro01@users.noreply.github.com>
2019-09-28 11:33:20 -0500

    kgjhghjvj,

--- controllers/RegisterController.php
+++ controllers/RegisterController.php
@@ -1,13 +1,7 @@
 <?php 
     class RegisterController extends Register{
-<<<<<<< HEAD
-        public function register(){
-        require_once('views/Register/index.php');
-    }
-=======
         public function index(){
-
-        }
->>>>>>> 5683b19c39ec97e7b399cd82ce770acfb3466fbf
+            require_once('views/Register/index.php');
+    }
     }
 ?>
