5f07bb8ed1c00590dd7d9e61fcac9166a4f70c55
Felipe Chacon <felipipe88806@gmail.com>
2019-09-28 11:44:34 -0500

    Add Login

--- views/login/index.php
+++ views/login/index.php
@@ -2,7 +2,19 @@
 <?php include('views/layouts/head.php')?>
 <body>
     
-    
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                
+            </div>
+            <div class="col-md-6">
+                <form action="">
+                    <label for=""></label>
+                </form>
+            </div>
+        </div>
+    </div>
+
 
 <?php include('views/layouts/footer.php') ?>
 </body>
